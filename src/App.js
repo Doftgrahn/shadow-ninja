a150c7f1e0229a790dda9fb35746786f895bef63
Simon grahn <simon.grahn@gmail.com>
2019-09-04 14:31:12 +0200

    initial push

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles/App.scss';
+
+const App = () => {
+    return (<div className="App">
+        <h1>Shadow Ninja</h1>
+    </div>);
+}
+
+export default App;
