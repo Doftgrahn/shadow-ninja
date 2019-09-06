7fa17d12c27d94b4ddaba2df0fc167512f666a6b
Maltex <henrik,nilsson88@gmail.com>
2019-09-06 11:15:20 +0200

    Authentication and Validation

--- api/server.js
+++ api/server.js
@@ -1,31 +1,46 @@
-const {insertMongoDB} = require('./database/AddProduct');
-const {getProductMongoDB} = require('./database/GetProduct');
 const express = require('express');
 const server = express();
+/*DATABASE*/
+const {insertMongoDB} = require('./database/AddProduct');
+const {getProductMongoDB} = require('./database/GetProduct');
+const db = require("./secrets/keys").mongoURI;
+/*DATABASE*/
+/* AUTH / LOGIN */
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/users");
+/* AUTH / LOGIN */
+
+server.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+server.use(bodyParser.json());
 
 
-// // function call for AddProduct
-// insertMongoDB()
-
-// // function call for getProduct
- // getProductMongoDB()
-
-
-server.use(express.static(__dirname + '/../build/'));
-server.use(express.json());
-
-server.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-});
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
 
 
-server.get('/test', (request, response) => {
-	console.log('Received GET request to /test');
-	response.send('Testing!');
-})
+
+
+// Passport middleware
+server.use(passport.initialize());
+// Passport config
+require("./secrets/passport")(passport);
+// Routes
+server.use("/api/users", users);
+
 
 server.get('/games', (request, response) => {
 	console.log('Received GET request to /test');
@@ -33,42 +48,10 @@ server.get('/games', (request, response) => {
 })
 
 
-
-// server.get('/word/', (req, res) => {
-// 	let searchWordMatched = [];
-//   if (req.query.sw) {
-// 		wordsData.filter(function(value, index, arr){
-// 			if (value.searchWord.includes(req.query.sw)) {
-// 				searchWordMatched.push(wordsData[index])
-// 			}
-// 			 else {
-// 				 console.log('No search matched database')
-// 			}
-// 		});
-// 			console.log(searchWordMatched)
-// 			res.send(JSON.stringify(searchWordMatched))
-//   } else {
-//     //Om det inte finns querystring som matchar retunera 404
-// 		res.send(fakeProducts)
-//   }
-// })
+server.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
 
-const port = process.env.PORT || 1337;
-server.listen(port, () => {
-	console.log('Server listening on port ' + port);
-})
 
-
-const product = {
-_id:'Lägger mongodb till automatiskt',
- title: 'STRING',
- category: 'STRING',
- price: 'NUMBER',
- rating: 'NUMBER',
- image: 'STRING',
- info: 'STRING'
-}
 
 const fakeProducts = [
   { _id:'Lägger mongodb till automatiskt', title: 'STRING', category: 'STRING', price: 'NUMBER', rating: 'NUMBER', image: 'STRING', info: 'STRING' },
