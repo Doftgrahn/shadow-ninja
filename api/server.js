b2550173858229d7d95b4bbfc455dfbad6b57937
Maltex <henrik,nilsson88@gmail.com>
2019-09-09 19:48:12 +0200

    refactor some code

--- api/server.js
+++ api/server.js
@@ -5,20 +5,38 @@ const {insertMongoDB} = require('./database/AddProduct');
 const {getProductMongoDB} = require('./database/GetProduct');
 const db = require("./secrets/keys").mongoURI;
 /*DATABASE*/
+
 /* AUTH / LOGIN */
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/users");
+require("./secrets/passport")(passport);
 /* AUTH / LOGIN */
 
+// 1 middleware
+/* Handle Auth / Login */
 server.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
 server.use(bodyParser.json());
+server.use(passport.initialize());
+/* Handle Auth / Login */
 
+// 2 Routes
+
+
+
+// 3 felhantering
+// 4 starta serven
+
+
+server.get('/games', (request, response) => {
+	console.log('Received GET request to /test');
+	response.send(fakeProducts);
+})
 
 // Connect to MongoDB
 mongoose
@@ -28,26 +46,15 @@ mongoose
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
 
-
-
-
-// Passport middleware
-server.use(passport.initialize());
-// Passport config
-require("./secrets/passport")(passport);
 // Routes
 server.use("/api/users", users);
 
 
-server.get('/games', (request, response) => {
-	console.log('Received GET request to /test');
-	response.send(fakeProducts);
-})
 
 
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 server.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
 
---
+++ api/settings/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: "mongodb+srv://shadow:1234@cluster0-p7r7i.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
+// URI som skall hantera loggin
+//secret key: secretOrKey skall bli en krypterad string
