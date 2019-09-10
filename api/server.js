b52601dff63db6ab0eeb2519a7bb0153a91e5f96
Niclas Hammarström <hammarstrom@Niclass-MacBook-Air.local>
2019-09-10 11:09:52 +0200

    express.static

--- api/server.js
+++ api/server.js
@@ -5,12 +5,16 @@ const {insertMongoDB} = require('./database/AddProduct');
 const {getProductMongoDB} = require('./database/GetProduct');
 const db = require("./secrets/keys").mongoURI;
 /*DATABASE*/
+
 /* AUTH / LOGIN */
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/users");
 /* AUTH / LOGIN */
+
+server.use(express.static(__dirname + '/../build/'));
+server.use(express.static(__dirname + '/../dist/'));
 
 server.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -32,7 +36,7 @@ server.use(
     extended: false
   })
 );
-server.use(bodyParser.json());
+
 
 // Connect to MongoDB
 mongoose
@@ -44,18 +48,13 @@ mongoose
   .catch(err => console.log(err));
 
 
-
-server.get('/', (request, response) => {
-	console.log('Received GET request to /test');
-	response.send('Testing!');
-})
-
 // function call for GetProduct from database
 server.get('/api/games', (request, response) => {
    getProductMongoDB(data => {
      response.send(JSON.stringify(data))
    })
 })
+
 
 // Passport middleware
 server.use(passport.initialize());
@@ -64,10 +63,18 @@ require("./secrets/passport")(passport);
 // Routes
 server.use("/api/users", users);
 
-server.get('/games', (request, response) => {
-	console.log('Received GET request to /test');
-	response.send(fakeProducts);
+server.get('/error', (req, res) => {
+	throw Error('User error');
 })
+
+server.use((error, request, response, next) => {
+  response.status(500).send('error 500 error')
+})
+
+// server.get('/games', (request, response) => {
+// 	console.log('Received GET request to /test');
+// 	response.send(fakeProducts);
+// })
 
 
 // server.get('/word/', (req, res) => {
