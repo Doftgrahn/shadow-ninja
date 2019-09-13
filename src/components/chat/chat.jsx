0749d1f447eab3efee7bd927c907b9f78b981948
Simon grahn <simon.grahn@gmail.com>
2019-09-13 14:34:02 +0200

    started with chat component

---
+++ src/components/chat/chat.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//import io from 'socket.io-client';
+
+//const socket = io();
+
+
+
+const Chat = () => {
+
+
+
+
+    return (<main className="chat">Chat</main>)
+}
+
+export default Chat;
--- src/components/miniMeny/miniMeny.jsx
+++ src/components/miniMeny/miniMeny.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import CartInfo from './cart/cartInfo';
+import Chat from '../chat/chat';
 
 const MiniMeny = ({cart}) => {
 
     return (<main>
         <CartInfo/>
+        <Chat/>
     </main>)
 }
 
