4d6485ac04f13232e3da7a744c5aa32f45872a0d
Samira Ghorbani <sghorbano@gmail.com>
2019-09-17 15:52:54 +0200

    some changes to the frontend webpage

--- api/secrets/mongodb.config.js
+++ api/secrets/mongodb.config.js
@@ -1,4 +1,6 @@
-const uri =
-  "mongodb+srv://shadow:1234@cluster0-p7r7i.mongodb.net/shadowDB?retryWrites=true&w=majority";
+const uri =  'mongodb+srv://shadow:1234@cluster0-p7r7i.mongodb.net/test?retryWrites=true&w=majority';
+
+
+
 
 module.exports = {uri};
