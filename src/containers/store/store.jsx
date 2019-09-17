98254c3539f8c431986cf6ef22f4f8675992fe61
oNilian <julian@alsen.se>
2019-09-17 15:15:59 +0200

    sökFält för spel flyttad från store till header

--- src/containers/store/store.jsx
+++ src/containers/store/store.jsx
@@ -11,8 +11,6 @@ import Loader from '../../components/loader/loader';
 import PromoGame from './components/promoGame/promoGame';
 import SortGames from './components/sortGames/sortGames';
 import AllGames from './components/allGames/allGames';
-//Navbar, in Progress
-import StoreNavbar from './components/storenavbar/storenavbar';
 
 // General Wrapper for GAMES
 const Store = ({dispatch, products, loading, error, match}) => {
@@ -40,7 +38,6 @@ const Store = ({dispatch, products, loading, error, match}) => {
     return (
     <main className="games">
         <PromoGame match={match} products={products}/>
-        <StoreNavbar/>
         <SortGames/>
         <AllGames products={products} match={match}/>
     </main>)
