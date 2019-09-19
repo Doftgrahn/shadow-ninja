8aba655f9a12a9dfe7927775d4dbd48e3297c170
oNilian <julian@alsen.se>
2019-09-19 11:17:03 +0200

    hotfix, navbar

--- src/containers/general/header/storenavbar/storenavbar.jsx
+++ src/containers/general/header/storenavbar/storenavbar.jsx
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
+import {Link} from "react-router-dom";
+
 import {connect} from 'react-redux';
+import oneGame from '../../../store/components/oneGame/oneGame';
 
-
-const StoreNavbar = ({products}) => {
+const StoreNavbar = ({products, match, gameinfo}) => {
     const [filterValue, setfilterValue] = useState('');
-
     // Sets state to the users input
     const changeFilterValue = (event) => {
         setfilterValue(event.target.value) 
@@ -18,10 +19,15 @@ const StoreNavbar = ({products}) => {
     // Makes a new list after when found titles
     const newArray = actualFilter.map(e => e.title)
 
+    //Function som skall känna av vad man tryckte på och matcha keyVärdet i mapObjektet för att sedan kunna länka vidare med hjälp av detta värdet man hittade
+    const selectedGameHandler = () => {
+        // console.log('ett spel är valt',limitedAutoComplete.find() )
+    }
     // Limits the new list to a max of 4 items to be suggested to the user
     const limitedAutoComplete = newArray.slice(0, 4).map(x => (
-        <li key={x}>{x}</li>
-    ))
+        <li onClick={selectedGameHandler}key={x}>{x}</li>
+        ))
+
 
     // Only shows the suggestions to user if there is something written by the user
     if(filterValue === '') {
