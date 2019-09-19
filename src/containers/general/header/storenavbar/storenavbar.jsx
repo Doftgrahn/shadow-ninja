2f8a3cc73bd44dc6a6bb06e03caf6bf089f7744c
oNilian <julian@alsen.se>
2019-09-19 12:07:50 +0200

    fungerande lnkar i storeNavbar

--- src/containers/general/header/storenavbar/storenavbar.jsx
+++ src/containers/general/header/storenavbar/storenavbar.jsx
@@ -2,10 +2,10 @@ import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 
 import {connect} from 'react-redux';
-import oneGame from '../../../store/components/oneGame/oneGame';
 
 const StoreNavbar = ({products, match, gameinfo}) => {
     const [filterValue, setfilterValue] = useState('');
+    
     // Sets state to the users input
     const changeFilterValue = (event) => {
         setfilterValue(event.target.value) 
@@ -17,15 +17,13 @@ const StoreNavbar = ({products, match, gameinfo}) => {
     })
     
     // Makes a new list after when found titles
-    const newArray = actualFilter.map(e => e.title)
+    const newArray = actualFilter.map(e => ({title: e.title, _id: e._id}))
 
-    //Function som skall känna av vad man tryckte på och matcha keyVärdet i mapObjektet för att sedan kunna länka vidare med hjälp av detta värdet man hittade
-    const selectedGameHandler = () => {
-        // console.log('ett spel är valt',limitedAutoComplete.find() )
-    }
     // Limits the new list to a max of 4 items to be suggested to the user
     const limitedAutoComplete = newArray.slice(0, 4).map(x => (
-        <li onClick={selectedGameHandler}key={x}>{x}</li>
+        <Link key={x._id} to={`/store/${x._id}`}>
+        <li>{x.title}</li>
+        </Link>
         ))
 
 
