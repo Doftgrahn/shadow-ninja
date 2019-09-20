9d2d78c974fe726a4188ce08dabcde2d45232862
mringen <n.hammarstrom@hotmail.se>
2019-09-20 19:17:06 +0200

    new file

---
+++ src/containers/store/components/filterCategory/filterCategory.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {connect, useDispatch} from "react-redux";
+
+import {setFilter} from '../../../../services/products/productActions';
+
+const FilterCategory = ({filter}) => {
+	const dispatch = useDispatch();
+	let categories = [
+		'All', 'Adventure', 'Action', 'Fighting', 'Puzzle', 'RPG', 'Sport', 'Strategy'
+	];
+
+	const renderCategory = categories.map((category, index) => {
+		return  <option key={index} value={category}>{category}</option>
+	})
+
+		//return send to sortGames.jsx
+		return(
+			<div className="filterCategory">
+			<span>Categories:</span>
+				<select value={filter} onChange={e => dispatch(setFilter(e.target.value))}>
+					{renderCategory}
+				</select>
+			</div>
+		)
+}
+
+const mapStateToProps = state => ({filter: state.products.filter})
+export default connect(mapStateToProps)(FilterCategory)
