90df244159f7a24c0938f5491e4b735307e966cb
Simon grahn <simon.grahn@gmail.com>
2019-09-24 13:50:50 +0200

    removed all traces from old router animations

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -11,10 +11,6 @@ import Login from '../containers/login-Handler/Login';
 import Profile from '../containers/userProfile/Profile'
 import PrivateRoute from "./PrivateRoute";
 
-
-// import AnimatedRoute from './animatedRoute';
-// import AnimatedSwitch from './animatedSwitch';
-
 const Routes = () => {
 
     const allRoutes = [
@@ -24,58 +20,46 @@ const Routes = () => {
             path: '/',
             Component: LandingPage
         }, {
-            _id:1,
+            _id: 1,
             exact: true,
             path: '/store',
             Component: Store
         }, {
-            _id:2,
+            _id: 2,
             exact: false,
             path: '/store/:id',
             Component: SingleGame
         }, {
-            _id:3,
+            _id: 3,
             exact: false,
             path: '/social',
             Component: Social
         }, {
-            _id:4,
+            _id: 4,
             exact: false,
             path: '/checkout',
             Component: Checkout
         }, {
-            _id:5,
+            _id: 5,
             exact: false,
             path: '/register',
             Component: Register
         }, {
-            _id:6,
+            _id: 6,
             exact: false,
             path: '/login',
             Component: Login
-        },
-
-
+        }
     ]
 
     const renderRoutes = allRoutes.map(r => {
-        return <Route
-                key={r._id}
-                exact={r.exact}
-                path={r.path}
-                render={(props) => <r.Component {...props}/>}/>
+        return <Route key={r._id} exact={r.exact} path={r.path} render={(props) => <r.Component {...props}/>}/>
     })
 
-    return ( <Switch
-        animationClassName="page-slide"
-        animationTimeout={300}
-      >
-          {renderRoutes}
-          <PrivateRoute exact path="/userProfile" component={Profile} />
-          </Switch>
-)
+    return (<Switch >
+        {renderRoutes}
+        <PrivateRoute exact="exact" path="/userProfile" component={Profile}/>
+    </Switch>)
 }
-
-
 
 export default Routes;
