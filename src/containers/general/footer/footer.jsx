0025226c5f53df61a6342f855dfdb3a7bbd5a9a7
Samira Ghorbani <sghorbano@gmail.com>
2019-09-26 11:08:28 +0200

    ny style

--- src/containers/general/footer/footer.jsx
+++ src/containers/general/footer/footer.jsx
@@ -4,13 +4,14 @@ import {SocialIcon} from 'react-social-icons';
 const Footer = () => {
     return (<footer className="footer">
         <div className="footer__wrapper">
+            
             <h4>Gmail adrese :<br/>
             </h4>
             <a className="a" href="intressantdokument.html#imail">ninja@jmail.com</a><br/><br/>
             <div className="iconWrapper">
-                <SocialIcon className="icon" url="http://twitter.com"/>
-                <SocialIcon className="icon" url="http://facebook.com"/>
-                <SocialIcon className="icon" url="http://instagram.com"/>
+                <SocialIcon className="icon" url="http://twitter.com" bgColor="#f7931e" fgColor="white"/>
+                <SocialIcon className="icon" url="http://facebook.com" bgColor="#f7931e" fgColor="white"/>
+                <SocialIcon className="icon" url="http://instagram.com" bgColor="#f7931e" fgColor="white"/>
             </div>
 
         </div>
--- src/containers/landingpage/landingpage.jsx
+++ src/containers/landingpage/landingpage.jsx
@@ -3,20 +3,23 @@ import {Link} from "react-router-dom";
 
 import Fade from 'react-reveal/Fade';
 
-import {ReactComponent as Hero} from '../../components/icon/shadow.svg';
+import {ReactComponent as Hero} from '../../components/icon/ninja page.svg';
 
 
 
 const LandingPage = () => {
     return (<main className="box">
+        
     <Fade>
     <Hero/>
 
 <div className="box-wrapper">
         <div className="title">
 
-            {<h1 className="big textCarosel"></h1>}
-
+            {<h1 className="big textCarosel"> 
+           </h1>}
+           
+           
             <Link to={'/store'}>
                 <button className="hover ">Store</button>
             </Link>
