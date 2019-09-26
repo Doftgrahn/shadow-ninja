06b8aaee47b8193702becbe28948617f3ababb8d
Simon grahn <simon.grahn@gmail.com>
2019-09-26 16:09:45 +0200

    puss

--- src/containers/landingpage/landingpage.jsx
+++ src/containers/landingpage/landingpage.jsx
@@ -5,9 +5,7 @@ import Fade from 'react-reveal/Fade';
 
 import {ReactComponent as Hero} from '../../components/icon/ninja_page.svg';
 
-
 import Glitch from '../../components/glitch/glitch';
-
 
 const LandingPage = () => {
     return (<main className="box">
@@ -17,8 +15,7 @@ const LandingPage = () => {
             </figure>
 
             <div className="title">
-                <Glitch/>
-                {/*<p className="big textCarosel"></p>*/}
+                <Glitch/> {/* <p className="big textCarosel"></p> */}
                 <Link to={'/store'}>
                     <button className="hover ">Store</button>
                 </Link>
