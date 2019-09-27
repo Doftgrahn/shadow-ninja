f7893045389643c3eddd6075b2e26b1f8aee7bc0
Maltex <henrik.nilsson88@gmail.com>
2019-09-27 10:53:05 +0200

    styling

--- src/components/pagenotfound/Pagenotfound.js
+++ src/components/pagenotfound/Pagenotfound.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './pagenotfound.scss';
 
 const PageNotFound = () => (
-<div>
-<p>Hej Error 404</p>
+<div className="Maindiv">
+
+<h1>Error 404</h1>
+<h3>Even ninjas get lost</h3>
+
 <center><Link to="/">Return to Home Page</Link></center>
 </div>
 );
