0829cf0d9f656362cd5ff5c47fef21aca656e273
Simon grahn <simon.grahn@gmail.com>
2019-09-30 11:26:49 +0200

    styling to hero and added font

---
+++ src/containers/landingpage/hero/hero.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+//import Glitch from '../../../components/glitch/glitch';
+
+import Fade from 'react-reveal/Fade';
+
+import {Link} from "react-router-dom";
+
+const Hero = () => {
+    return (<section className="hero">
+
+        <div className="mainText">
+            {/*<Glitch/>*/}
+            <Fade right={true}>
+            <h1>Shadow Ninja.</h1>
+            </Fade>
+
+        </div>
+
+        <div className="toStoreBtn">
+            <Link to={'/store'}>
+                <button className="hover ">Store</button>
+            </Link>
+
+        </div>
+
+    </section>)
+}
+
+export default Hero;
--- src/containers/landingpage/landingpage.jsx
+++ src/containers/landingpage/landingpage.jsx
@@ -1,34 +1,17 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 
 import Fade from 'react-reveal/Fade';
 
-import {ReactComponent as Hero} from '../../components/icon/ninja_page.svg';
-
 //import Glitch from '../../components/glitch/glitch';
+import Hero from './hero/hero';
 import Block from './infoBlock/block';
 import PromoBlock from './promoBlock/promoBlock';
 import PromoChat from './promoChat/promoChat';
 
-
 const LandingPage = () => {
     return (<main className="landingpage">
         <Fade>
-            <div className="box">
-                <figure>
-                    <Hero/>
-                </figure>
-
-                <div className="title">
-                    {/* <Glitch/> */}
-                    {<p className="big textCarosel"></p>}
-                    <Link to={'/store'}>
-                        <button className="hover ">Store</button>
-                    </Link>
-
-                </div>
-
-            </div>
+            <Hero/>
             <Block/>
             <PromoBlock/>
             <PromoChat/>
